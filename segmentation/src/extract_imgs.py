8affee1a30e702e5cc53049d9f6fcdeba1301e7d
Justus Schock <justus.schock@rwth-aachen.de>
2017-10-28 17:52:19 +0200

    changed file paths

--- segmentation/src/extract_imgs.py
+++ segmentation/src/extract_imgs.py
@@ -7,8 +7,8 @@ edge_upper_left = (0, 0)
 edge_middle = (0, 0)
 edge_bottom_right = (0, 0)
 
-file_name = ""
-out_dir = ""
+file_name = "/home/temp/schock/MGS/MVI_0315.MOV"
+out_dir = "/home/temp/schock/MGS/" + os.path.split(file_name)[-1].rsplit(".")[0]
 
 frames = []
 
@@ -20,13 +20,13 @@ count = 0
 success = True
 while success:
     success, image = vidcap.read()
-    if count == 0:
-        tmp = input("Please enter coordinates of upper left edge (separated by \",\"):").split(",")
-        edge_upper_left = (int(tmp[0]), int(tmp[1]))
-        tmp = input("Please enter coordinates of middle edge (separated by \",\"):").split(",")
-        edge_middle = (int(tmp[0]), int(tmp[1]))
-        tmp = input("Please enter coordinates of bottom right edge (separated by \",\"):").split(",")
-        edge_bottom_right = (int(tmp[0]), int(tmp[1]))
+    # if count == 0:
+    #   tmp = input("Please enter coordinates of upper left edge (separated by \",\"):").split(",")
+    #   edge_upper_left = (int(tmp[0]), int(tmp[1]))
+    #   tmp = input("Please enter coordinates of middle edge (separated by \",\"):").split(",")
+    #   edge_middle = (int(tmp[0]), int(tmp[1]))
+    #   tmp = input("Please enter coordinates of bottom right edge (separated by \",\"):").split(",")
+    #   edge_bottom_right = (int(tmp[0]), int(tmp[1]))
 
     cv2.imwrite(os.path.join(out_dir, "frame%d.jpg" % count), image)     # save frame as JPEG file
     if cv2.waitKey(10) == 27:                     # exit if Escape is hit
--- segmentation/src/saliency_map.py
+++ segmentation/src/saliency_map.py
@@ -4,15 +4,16 @@ import torchvision.transforms as transforms
 
 import numpy as np
 from PIL import Image
+import os
 
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 
 import json
-imagenet_classes = json.load(open('imagenet_classes.json'))
-idx2class = [imagenet_classes[str(i)].split(',')[0] for i in range(1000)]
-class2idx = {v:i for i,v in enumerate(idx2class)}
+#imagenet_classes = json.load(open('imagenet_classes.json'))
+#idx2class = [imagenet_classes[str(i)].split(',')[0] for i in range(1000)]
+#class2idx = {v:i for i,v in enumerate(idx2class)}
 
 vgg16 = models.vgg16(pretrained=True)
 vgg16.eval()
@@ -21,16 +22,20 @@ print(vgg16)
 img_transforms = transforms.Compose([transforms.Scale((224, 224), Image.BICUBIC),
                                      transforms.ToTensor(),
                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+
 def unnorm(x):
     un_x = 255*(x*0.5+0.5)
     return un_x.astype(np.uint8)
 
-img = Image.open('beagle.jpg')
+
+base_path = "/home/schock/Documents/mouse-project/segmentation"
+img = Image.open('mouse.png').convert('RGB')
 img_tensor = img_transforms(img)
 
 img_var = torch.autograd.Variable(img_tensor.unsqueeze(0), requires_grad=True)
 out = vgg16(img_var)
-print(str(np.argmax(out.data.numpy())) + ":" + idx2class[np.argmax(out.data.numpy())])
+#print(str(np.argmax(out.data.numpy())) + ":" + idx2class[np.argmax(out.data.numpy())])
 
 criterion = torch.nn.CrossEntropyLoss()
 label = torch.autograd.Variable(torch.LongTensor(np.array([11])))
@@ -43,7 +48,7 @@ plt.imshow(unnorm(img_tensor.numpy()).transpose(1,2,0))
 plt.subplot(1,2,2)
 grads = np.abs(img_var.grad.data.numpy()[0]).max(axis=0)
 plt.imshow(np.abs(img_var.grad.data.numpy()[0]).max(axis=0), cmap='gray')
-fig.savefig("/home/students/schock/mouse-project/segmentation/test.png")
+fig.savefig(os.path.join(base_path, "test.png"))
 
 img_seg = np.asarray(img.resize((224,224)))
 seg_mask = np.zeros_like(img_seg)
@@ -56,6 +61,6 @@ plt.subplot(1,2,1)
 plt.imshow(np.asarray(img.resize((224,224))))
 plt.subplot(1,2,2)
 plt.imshow(img_seg)
-fig.savefig("/home/students/schock/mouse-project/segmentation/test_seg.png")
+fig.savefig(os.path.join(base_path, "test_seg.png"))
 
 print("")
